1c8f346469aca20ecb949b5fb9f11b982c85aff5
Adefolarin Osu <aosu@teamapt.com>
2019-12-30 07:47:01 +0100

    initial commit

---
+++ src/test/java/com/phos/springbootwebapplication/SpringbootwebapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phos.springbootwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwebapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
